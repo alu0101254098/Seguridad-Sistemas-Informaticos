f441def6e955fa9bff3ccf4a840087fe27f9190e
Jonathan Martinez <alu0101254098@ull.edu.es>
2023-03-07 10:49:26 +0100

    Añadiendo aesnow 3g

--- aesnow/aesnow.cc
+++ aesnow/aesnow.cc
@@ -1,10 +1,49 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <bitset>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <bits/stdc++.h> 
+#include <string.h>
+#include <iomanip>
+#include <utility>
 
 using namespace std;
 
 typedef bitset<8> Byte;
 const Byte AES_byte  = Byte(string("00011011"));
 const Byte SNOW_byte = Byte(string("10101001"));
+
+string HexToBin(string cadena) {
+  string resultado;
+  for (int i = 0; i < cadena.size(); i++){
+    switch (cadena[i]){
+      case 'a':
+        resultado += bitset<4>(10).to_string();
+        break;
+      case 'b':
+        resultado += bitset<4>(11).to_string();
+        break;
+      case 'c':
+        resultado += bitset<4>(12).to_string();
+        break;
+      case 'd':
+        resultado += bitset<4>(13).to_string();
+        break;
+      case 'e':
+        resultado += bitset<4>(14).to_string();
+        break;
+      case 'f':
+        resultado += bitset<4>(15).to_string();
+        break;
+      default:
+        resultado += bitset<4>(cadena[i]).to_string();
+    }
+  }
+  return resultado;
+}
 
 Byte mult(Byte b1, int pos, Byte Const) {
   cout << "――――――――――――――――――――――――――――――" << endl;
@@ -52,13 +91,23 @@ int main (void){
     cout << "0) Salir" << endl;
     cout << "--------------------" << endl;
     cin >> opt;
+    string b1, b2;
+
     switch (opt){
       case 1:{
-        cout << full_mult(Byte(string("01010111")), Byte(string("10000011")), AES_byte) << endl;
+        cout << "b1" << endl;
+        cin >> b1;
+        cout << "b2" << endl;
+        cin >> b2;
+        cout << full_mult(Byte(HexToBin("e1")), Byte(HexToBin("92")), AES_byte) << endl;
         break;
       }
       case 2:{
-        cout << full_mult(Byte(string("01010111")), Byte(string("10000011")), SNOW_byte) << endl;
+        cout << "b1" << endl;
+        cin >> b1;
+        cout << "b2" << endl;
+        cin >> b2;
+        cout << full_mult(Byte(HexToBin(b1)), (Byte(HexToBin(b1))), SNOW_byte) << endl;
         break;
       }
       case 0:
